b6c904f7a9c9f93f7e83650c8fd2b1fbb8f10802
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-08-27 01:06:18 +0300

    add slider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/ImgSlider.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function ImgSlider() {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  }
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src='/images/slider-badging.jpg' alt='' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/slider-badag.jpg' alt='' />
+      </Wrap>
+    </Carousel>
+  )
+}
+
+export default ImgSlider
+
+
+const Carousel = styled(Slider)`
+margin-top:30px;
+
+
+ul li button {
+  &:before {
+    font-size:10px;
+    color:rgb(150,158,171);
+  }
+}
+
+li.slick-active button:before {
+  color:white;
+}
+
+.slick-list {
+  overflow: visible;
+  
+}
+
+.slick-dots {
+  bottom:15px;
+  text-align:right;
+  padding-right:25px;
+}
+
+button {
+  z-index:1;
+}
+
+`
+
+const Wrap = styled.div`
+cursor:pointer;
+padding: 0 10px;
+
+img {
+  border: 4px solid transparent;
+  width:100%;
+  height:100%;
+  border-radius:4px;
+  box-shadow:rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px ;
+  transition-duration: 300ms;
+  
+
+      &:hover {
+        border: 4px solid rgba(249,249,249, 0.8);
+      }
+  } 
+
+`
